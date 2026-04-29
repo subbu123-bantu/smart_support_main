d3d11049277c7f52d61dd3f4d74c0701f8f46851
subbu <bantusubbu9@gmai.lcom>
2026-04-29 14:15:29 +0530

    changed index.js to show toasts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="top-right" autoClose={3000} />
   </React.StrictMode>
 );
 
